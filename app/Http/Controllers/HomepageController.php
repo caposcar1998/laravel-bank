a2eccfbfd2e322532491532d138ad524e7680e4e
Alejandro Flores <alejandro@esporaestudio.com>
2021-08-20 19:58:32 -0500

    Se agregó CoreUI al sistema

--- app/Http/Controllers/HomepageController.php
+++ app/Http/Controllers/HomepageController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 class HomepageController extends Controller
 {
     public function index() {
+        \Log::info("Este es  mi mensaje");
         $account = \App\Models\Account::find(1);
         return view('homepage.index', ['account' => $account]);
     }
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laboratorio</title>
+    <link rel="stylesheet" href="https://unpkg.com/@coreui/coreui/dist/css/coreui.min.css">
+</head>
+<body>
+    @yield('content')
+    <script src="https://unpkg.com/@coreui/coreui/dist/js/coreui.bundle.min.js"></script>
+</body>
+</html>
