330a9b99aa8357b94cd2e3f965f9d69e4d292300
Alejandro Flores <alejandro@esporaestudio.com>
2021-08-27 16:15:32 -0500

    Permite registrar un usuario (#3)

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,44 +1,71 @@
 @extends('layouts.main')
 
-@section('content')
+@section('title')
+    Registro
+@endsection
 
-<div class="row">
-    <div class="col-12">
-        <h1>Registro</h1>
-        @if ($errors->any())
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
-        <form action="{{ route('auth.save-user') }}" method="POST">
-            @csrf
-            <div class="form-group">
-                <label for="name">Nombre</label>
-                <input type="text" name="name" class="form-control" id="name" placeholder="John Doe">
-            </div>
-            <div class="form-group">
-                <label for="email">Email</label>
-                <input type="email" name="email" class="form-control" id="email" placeholder="name@example.com">
-            </div>
-            <div class="form-group">
-                <label for="password">Contraseña</label>
-                <input type="password" name="password" class="form-control" id="password">
-            </div>
-            <div class="form-group">
-                <label for="password_confirmation">Confirmar contraseña</label>
-                <input type="password" name="password_confirmation" class="form-control" id="password_confirmation">
-            </div>
-            <div class="form-group row">
-                <div class="col-sm-10">
-                    <button type="submit" class="btn btn-primary">Registrar</button>
+@section('layout_body_class_append')
+align-items-center
+@endsection
+
+@section('content')
+<div class="row justify-content-center">
+    <div class="col-md-6">
+        <div class="card mx-4">
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
                 </div>
+            @endif
+            <div class="card-body p-4">
+                <h1>Registrate</h1>
+                <p class="text-muted">
+                    Crea tu cuenta aquí
+                </p>
+                <form action="{{ route('auth.save-user') }}" method="POST">
+                    @csrf
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">
+                                <i class="cil-user"></i>
+                            </span>
+                        </div>
+                        <input class="form-control" name="name" type="text" placeholder="Nombre">
+                    </div>
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">
+                                <i class="cil-envelope-open"></i>
+                            </span>
+                        </div>
+                        <input class="form-control" name="email" type="text" placeholder="Email">
+                    </div>
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">
+                                <i class="cil-lock-locked"></i>
+                            </span>
+                        </div>
+                        <input class="form-control" name="password" type="password" placeholder="Contraseña">
+                    </div>
+                    <div class="input-group mb-4">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">
+                                <i class="cil-lock-locked"></i>
+                            </span>
+                        </div>
+                        <input class="form-control" name="password_confirmation" type="password" placeholder="Confirma la contraseña">
+                    </div>
+                    <button class="btn btn-block btn-success" type="submit">
+                        Registrarse
+                    </button>
+                </form>
             </div>
-        </form>
+        </div>
     </div>
 </div>
-
 @endsection
